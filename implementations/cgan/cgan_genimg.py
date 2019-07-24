b81c5357cea9598de5e35f7e7c72395003639676
rombaux <rombaux@hotmail.com>
2019-07-24 21:16:27 +0200

    test genimg

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -210,4 +210,4 @@ for epoch in range(opt.n_epochs):
 
         batches_done = epoch * len(dataloader) + i
         if batches_done % opt.sample_interval == 0:
-            sample_image(n_row=10, batches_done=batches_done, date_string=date_string)
+            sample_image(n_row=1, batches_done=batches_done, date_string=date_string)
