eae4297b5a2834c148adda422f04d495c7d6c87d
rombaux <rombaux@hotmail.com>
2019-07-24 21:26:26 +0200

    test l1

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -139,7 +139,7 @@ def sample_image(n_row, batches_done,date_string):
     # Sample noise
     z = Variable(FloatTensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
     # Get labels ranging from 0 to n_classes for n rows
-    labels = np.array(opt.genidlabel)
+    labels = np.array([1])
     # labels = np.array([num for _ in range(n_row) for num in range(n_row)])
     labels = Variable(LongTensor(labels))
     gen_imgs = generator(z, labels)
