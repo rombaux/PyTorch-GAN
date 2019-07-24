4e7df9f24e7691b1fc75a32a254ed7e781f0e50a
rombaux <rombaux@hotmail.com>
2019-07-24 21:29:00 +0200

    gen1

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -137,7 +137,8 @@ LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 def sample_image(n_row, batches_done,date_string):
     """Saves a grid of generated digits ranging from 0 to n_classes"""
     # Sample noise
-    z = Variable(FloatTensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
+    #z = Variable(FloatTensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
+    z = Variable(FloatTensor(np.random.normal(0, 1, (1, opt.latent_dim))))
     # Get labels ranging from 0 to n_classes for n rows
     labels = np.array([1])
     # labels = np.array([num for _ in range(n_row) for num in range(n_row)])
