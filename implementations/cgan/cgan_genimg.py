713372dc51f6e824e51f6eac969f53333b320cb7
rombaux <rombaux@hotmail.com>
2019-07-24 22:49:13 +0200

    t nombre

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -155,7 +155,7 @@ def sample_label_id_image(n_row, batches_done,date_string):
     gen_imgs = generator(z, labels)
  #   save_image(gen_imgs.data[opt.genidlabel],  "/content/gdrive/My Drive/TFE/images/"+date_string+"/gen_"+str(opt.genidlabel)+"_%d.png" % batches_done, nrow=n_row, normalize=True)
 
-    save_image(gen_imgs.data[842],  "/content/gdrive/My Drive/TFE/images/"+date_string+"/gen_"+842)+"_%d.png" % batches_done, nrow=n_row, normalize=True)
+    save_image(gen_imgs.data[8,4,2],  "/content/gdrive/My Drive/TFE/images/"+date_string+"/gen_"+842)+"_%d.png" % batches_done, nrow=n_row, normalize=True)
 
 
 # ----------
