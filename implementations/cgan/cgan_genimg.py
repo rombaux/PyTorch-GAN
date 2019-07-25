56f3a28d519fd01fc4640e59ce92f2a425cdebc9
rombaux <rombaux@hotmail.com>
2019-07-25 23:09:50 +0200

    efe

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -47,46 +47,6 @@ pathimage = os.path.join("/content/gdrive/My Drive/TFE/images/",date_string)
 os.mkdir(pathimage)
 print ("Path is created as " + pathimage)
 
-# ----------
-#  Model
-# ----------
-
-# Define model
-class TheModelClass ( nn . Module ):
-    def __init__ ( self ):
-        super ( TheModelClass , self ) . __init__ ()
-        self . conv1 = nn . Conv2d ( 3 , 6 , 5 )
-        self . pool = nn . MaxPool2d ( 2 , 2 )
-        self . conv2 = nn . Conv2d ( 6 , 16 , 5 )
-        self . fc1 = nn . Linear ( 16 * 5 * 5 , 120 )
-        self . fc2 = nn . Linear ( 120 , 84 )
-        self . fc3 = nn . Linear ( 84 , 10 )
-
-    def forward ( self , x ):
-        x = self . pool ( F . relu ( self . conv1 ( x )))
-        x = self . pool ( F . relu ( self . conv2 ( x )))
-        x = x . view ( - 1 , 16 * 5 * 5 )
-        x = F . relu ( self . fc1 ( x ))
-        x = F . relu ( self . fc2 ( x ))
-        x = self . fc3 ( x )
-        return x
-
-# Initialize model
-model = TheModelClass ()
-
-# Initialize optimizer
-optimizer = optim.SGD ( model . parameters (), lr = 0.001 , momentum = 0.9 )
-
-# Print model's state_dict
-print ( "Model's state_dict:" )
-for param_tensor in model . state_dict ():
-    print ( param_tensor , " \t " , model . state_dict ()[ param_tensor ] . size ())
-
-# Print optimizer's state_dict
-print ( "Optimizer's state_dict:" )
-for var_name in optimizer . state_dict ():
-    print ( var_name , " \t " , optimizer . state_dict ()[ var_name ])
-
 
 
 class Generator(nn.Module):
@@ -110,8 +70,18 @@ class Generator(nn.Module):
             nn.Linear(1024, int(np.prod(img_shape))),
             nn.Tanh()
         )
-        PATCH = "/content/gdrive/My Drive/TFE/model/model.pth"
-        torch.save(model.state_dict(), PATCH)   
+        PATCH = "/content/gdrive/My Drive/TFE/model/modelg.pth"
+        torch.save(model.state_dict(), PATCH)  
+
+        # Print model's state_dict
+        print ( "Model's state_dict:" )
+        for param_tensor in model . state_dict ():
+        print ( param_tensor , " \t " , model . state_dict ()[ param_tensor ] . size ())
+
+        # Print optimizer's state_dict
+        print ( "Optimizer's state_dict:" )
+        for var_name in optimizer . state_dict ():
+        print ( var_name , " \t " , optimizer . state_dict ()[ var_name ])
 
 
     def forward(self, noise, labels):
