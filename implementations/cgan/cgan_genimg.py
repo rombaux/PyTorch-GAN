7febaf351ce7cc713b9f7dc416365ef761394f22
rombaux <rombaux@hotmail.com>
2019-07-25 23:11:02 +0200

    gtg

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -74,14 +74,14 @@ class Generator(nn.Module):
         torch.save(model.state_dict(), PATCH)  
 
         # Print model's state_dict
-        print ( "Model's state_dict:" )
+        print( "Model's state_dict:" )
         for param_tensor in model . state_dict ():
-        print ( param_tensor , " \t " , model . state_dict ()[ param_tensor ] . size ())
+            print(param_tensor , " \t " , model . state_dict ()[ param_tensor ] . size ())
 
         # Print optimizer's state_dict
         print ( "Optimizer's state_dict:" )
         for var_name in optimizer . state_dict ():
-        print ( var_name , " \t " , optimizer . state_dict ()[ var_name ])
+            print(var_name , " \t " , optimizer . state_dict ()[ var_name ])
 
 
     def forward(self, noise, labels):
