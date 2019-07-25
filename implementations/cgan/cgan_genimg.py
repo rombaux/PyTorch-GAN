ae9e91338b7fb033f2cf0333e216a36a7ff2a942
rombaux <rombaux@hotmail.com>
2019-07-25 23:17:22 +0200

    ytry

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -238,5 +238,7 @@ for epoch in range(opt.n_epochs):
             print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item()))
             sample_image(n_row=10, batches_done=batches_done, date_string=date_string)
             sample_label_id_image(n_row=10, batches_done=batches_done, date_string=date_string)
+            PATCH = "/content/gdrive/My Drive/TFE/model/modelg.pth"
+            torch.save(Generator.model.state_dict(), PATCH)  
                      
 
