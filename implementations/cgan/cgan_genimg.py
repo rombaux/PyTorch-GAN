f074071a13565f8718b0ad90233e1a2d27cbcbcd
rombaux <rombaux@hotmail.com>
2019-07-26 01:56:13 +0200

    hjk

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -47,12 +47,12 @@ pathimage = os.path.join(os.path.sep,'content','gdrive','My Drive','TFE','images
 print ("Path is created as " + pathimage)
 os.makedirs(pathimage)
 
-pathimage = os.path.join(os.path.sep,'content','gdrive','My Drive','TFE','images',date_string,'gen',str(opt.genidlabel))
+pathimage = os.path.join(os.path.sep,'content','gdrive','My Drive','TFE','images',date_string,str(opt.genidlabel))
 print ("Path is created as " + pathimage)
 os.makedirs(pathimage)
 
 
-pathimage = os.path.join(os.path.sep,'content','gdrive','My Drive','TFE','images',date_string,'gen',str(opt.gennumber))
+pathimage = os.path.join(os.path.sep,'content','gdrive','My Drive','TFE','images',date_string,str(opt.gennumber))
 print ("Path is created as " + pathimage)
 os.makedirs(pathimage)
 
@@ -191,13 +191,13 @@ def sample_label_id_image(n_row, batches_done,date_string):
     labels = Variable(LongTensor(labels))
     gen_imgs = generator(z, labels)
     if opt.genidlabel < 10:
-        save_image(gen_imgs.data[opt.genidlabel],  "/content/gdrive/My Drive/TFE/images/"+date_string+"/gen"+str(opt.genidlabel)+"/gen_"+str(opt.genidlabel)+"_%d.png" % batches_done, nrow=n_row, normalize=True)
+        save_image(gen_imgs.data[opt.genidlabel],  "/content/gdrive/My Drive/TFE/images/"+date_string+"/"+str(opt.genidlabel)+"/gen_"+str(opt.genidlabel)+"_%d.png" % batches_done, nrow=n_row, normalize=True)
     if opt.gennumber > 0:
         toto = opt.gennumber
         numbre =[]
         for a in str(toto):
             numbre.append(gen_imgs.data[int(a)])
-        save_image(numbre,  "/content/gdrive/My Drive/TFE/images/"+date_string+"/gen_"+str(opt.gennumber)+"/gen"+str(opt.gennumber)+"_%d.png" % batches_done, nrow=n_row, normalize=True)
+        save_image(numbre,  "/content/gdrive/My Drive/TFE/images/"+date_string+"/gen_"+str(opt.gennumber)+"/"+str(opt.gennumber)+"_%d.png" % batches_done, nrow=n_row, normalize=True)
         print("nombre : "+str(opt.gennumber)+" generated")
 
     PATCH = "/content/gdrive/My Drive/TFE/model/model_"+str(batches_done)+".pth"
