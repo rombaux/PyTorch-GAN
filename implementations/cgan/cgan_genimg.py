00440ef5a1d93585e557d3ea6691cc3f2f1d957b
rombaux <rombaux@hotmail.com>
2019-07-25 23:22:35 +0200

    hghgh

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -157,7 +157,7 @@ def sample_image(n_row, batches_done,date_string):
     save_image(gen_imgs.data,  "/content/gdrive/My Drive/TFE/images/"+date_string+"/%d.png" % batches_done, nrow=n_row, normalize=True)
 
 
-def sample_label_id_image(n_row, batches_done,date_string,Generator):
+def sample_label_id_image(n_row, batches_done,date_string,generator):
     """Saves a grid of generated digits ranging from 0 to n_classes"""
     # Sample noise
     z = Variable(FloatTensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
@@ -176,7 +176,7 @@ def sample_label_id_image(n_row, batches_done,date_string,Generator):
         print("nombre : "+str(opt.gennumber)+" generated")
 
     PATCH = "/content/gdrive/My Drive/TFE/model/modelg.pth"
-    torch.save(Generator.model.state_dict(), PATCH)  
+    torch.save(generator.model.state_dict(), PATCH)  
 
 # ----------
 #  Training
@@ -238,7 +238,7 @@ for epoch in range(opt.n_epochs):
         batches_done = epoch * len(dataloader) + i
         if batches_done % opt.sample_interval == 0:
             print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item()))
-            sample_image(n_row=10, batches_done=batches_done, date_string=date_string,Generator=Generator)
+            sample_image(n_row=10, batches_done=batches_done, date_string=date_string,generator=generator)
             sample_label_id_image(n_row=10, batches_done=batches_done, date_string=date_string)
                     
 
