6ed7547dc8d784bf3fa104bafa651e39b22471c9
rombaux <rombaux@hotmail.com>
2019-08-01 22:55:32 +0200

    sds

--- implementations/cgan/cgan_loadgenimg.py
+++ implementations/cgan/cgan_loadgenimg.py
@@ -238,7 +238,7 @@ def sample_label_id_image(n_row, batches_done,date_string):
 PATH = "/content/gdrive/My Drive/TFE/model/model_dataset0_54049.pth"
 device = torch.device("cuda")
 model = Generator()
-model.load_state_dict(torch.load(PATH,strict=False))
+model.load_state_dict(torch.load(PATH))
 model.to(device)
 sample_image(n_row=opt.n_classes, batches_done=batches_done, date_string=date_string)
 
