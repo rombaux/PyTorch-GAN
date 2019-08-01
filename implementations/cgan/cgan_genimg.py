a65296d3c1756f54d719bbcba36aeb6e8b78f76d
rombaux <rombaux@hotmail.com>
2019-08-02 01:36:54 +0200

    sds

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -306,7 +306,7 @@ for epoch in range(opt.n_epochs):
             sample_label_id_image(n_row=opt.n_classes, batches_done=batches_done, date_string=date_string)
 
     PATCH = "/content/gdrive/My Drive/TFE/dataset/"+str(opt.dataset)+"/"+date_string+"/model/"+"model_" + str(batches_done) + ".pth"
-    torch.save(generator.state_dict(), PATCH)
+    torch.save(generator.model.state_dict(), PATCH)
     print("Model saved in "+str(PATCH))           
                     
 
