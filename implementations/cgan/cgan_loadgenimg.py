3220463f089f5cf5fea80ae733749bdfa9be9191
rombaux <rombaux@hotmail.com>
2019-08-02 09:29:55 +0200

    fgfg

--- implementations/cgan/cgan_loadgenimg.py
+++ implementations/cgan/cgan_loadgenimg.py
@@ -197,13 +197,13 @@ FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 
 
-def sample_image(n_row, batches_done,date_string,model_weights):
+def sample_image(n_row, batches_done,date_string,collections.OrderedDict):
     """Saves a grid of generated digits ranging from 0 to n_classes"""
     # Sample noise
     z = Variable(FloatTensor(np.random.normal(0.5, 0.5, (n_row ** 2, opt.latent_dim))))
     # Get labels ranging from 0 to n_classes for n rows
     #labels = np.array([num for _ in range(n_row) for num in range(n_row)])
-    labels = Variable(LongTensor(model_weights))
+    labels = Variable(LongTensor(collections.OrderedDict))
     gen_imgs = generator(z, labels)
     save_image(gen_imgs.data,  "/content/gdrive/My Drive/TFE/dataset/"+str(opt.dataset)+"/"+date_string+"/modelimage/%d.png" % batches_done, nrow=n_row, normalize=True)
 
@@ -225,7 +225,7 @@ model.to(device)
 '''
 
 print("Generation image")
-sample_image(n_row=opt.n_classes, batches_done=1, date_string=date_string,model_weights=model_weights)
+sample_image(n_row=opt.n_classes, batches_done=1, date_string=date_string,collections.OrderedDict=collections.OrderedDict)
 print("Image generee")
 
 # torch.save(generator.model.state_dict(), PATCH)
