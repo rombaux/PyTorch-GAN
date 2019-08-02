b0218efc7693923f54067d994af6451e7944ef16
rombaux <rombaux@hotmail.com>
2019-08-02 03:11:08 +0200

    z = 0

--- implementations/cgan/cgan_loadgenimg.py
+++ implementations/cgan/cgan_loadgenimg.py
@@ -201,10 +201,11 @@ def sample_image(n_row, batches_done,date_string):
     """Saves a grid of generated digits ranging from 0 to n_classes"""
     # Sample noise
     # z = Variable(FloatTensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
+    z = 0
     # Get labels ranging from 0 to n_classes for n rows
     labels = np.array([num for _ in range(n_row) for num in range(n_row)])
     labels = Variable(LongTensor(labels))
-    gen_imgs = generator(labels)
+    gen_imgs = generator(z, labels)
     save_image(gen_imgs.data,  "/content/gdrive/My Drive/TFE/dataset/"+str(opt.dataset)+"/"+date_string+"/modelimage/%d.png" % batches_done, nrow=n_row, normalize=True)
 
 
