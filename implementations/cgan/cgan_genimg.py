2fff449adff86da2c4636385f5cd7bb36dd6fda3
rombaux <rombaux@hotmail.com>
2019-08-04 00:49:51 +0200

    jhjh

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -193,10 +193,10 @@ if opt.dataset == 3:
         datasets.STL10(
             "../../data/STL10",
             split='train',
-            ttransform=transforms.Compose(
+            transform=transforms.Compose(
                 [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize(( 0.5 , 0.5 , 0.5 ), ( 0.5 , 0.5 , 0.5 ))]
             ),
-            target_transform=None,
+            #target_transform=None,
             download=True
             ),
         
