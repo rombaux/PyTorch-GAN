d4c22983fff1027fe334398063b1f098134271fe
rombaux <rombaux@hotmail.com>
2019-08-05 23:55:45 +0200

    fdfd

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -254,7 +254,11 @@ if opt.dataset == 6:            # DATASET EMNIST
             "../../data/EMNIST",
             split='byclass',
             transform=transforms.Compose(
-                [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize((0.5,),(0.5,))]
+                [transforms.Resize(opt.img_size),
+                lambda img: torchvision.transforms.functional.rotate(img, -90),
+                lambda img: torchvision.transforms.functional.hflip(img),
+                torchvision.transforms.ToTensor(),
+                transforms.Normalize((0.5,),(0.5,))]
             ),
             #target_transform=None,
             download=True
