1fa4336f0d096bd927ca740120e262d55d9fc3e2
rombaux <rombaux@hotmail.com>
2019-08-06 01:57:26 +0200

    gfg

---
+++ implementations/cgan/printloss.py
@@ -0,0 +1,18 @@
+import os, sys
+fn = []
+pathmodel = "/content/gdrive/My Drive/TFE/dataset/"
+for base, dirs, files in os.walk(pathmodel):
+        for file in files:
+            fn.append(os.path.join(base, file))
+print("Recherche dans : " + pathmodel + "\n\r") 
+fileList = [name for name in fn if name.endswith("loss.png")]
+
+for cnt, fileName in enumerate(fileList, 0):
+    print("[%d] %s" % (cnt, fileName))
+
+#choice = int(input("Choisissez le modèle à tester [0-%s]: " % cnt))
+
+from IPython.display import Image
+print('Evolution')
+
+display(Image(filename=fileList[cnt]))
