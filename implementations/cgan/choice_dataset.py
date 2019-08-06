a965ee98208f3c244667a8ba05819b8b8a8d74f6
rombaux <rombaux@hotmail.com>
2019-08-06 16:30:25 +0200

    ddf

--- implementations/cgan/choice_dataset.py
+++ implementations/cgan/choice_dataset.py
@@ -1,10 +1,21 @@
+print("\r\n")
+print("-------------------------------------------------")
+print("BIENVENUE SUR LA PAGE DES RESEAUX CONDITIONAL GAN")    
+print("-------------------------------------------------")   
+print("\r\n")
+print("\r\n")
+print("-------------------------------------------------")
+print("---           MENU DE CONFIGURATION           ---")    
+print("-------------------------------------------------")   
+print("\r\n")
+
 DatasetList = [u"dataset 0 - MNIST",u"dataset 1 - CIFAR 10",u"dataset 2 - CIFAR 100",u"dataset 3 - STL 10",u"dataset 4 - Fashion MNIST",u"dataset 5 - ImageNet",u"dataset 6 - EMNIST"]
 Batch_sizeList = [u"4",u"8",u"16",u"32",u"64",u"128",u"256",u"512",u"1024",u"2048"]
 Labeldataset1List = [u"Chiffre 0",u"Chiffre 1",u"Chiffre 2",u"Chiffre 3",u"Chiffre 4",u"Chiffre 5",u"Chiffre 6",u"Chiffre 7",u"Chiffre 8",u"Chiffre 9"]
 
 for cnt,listedonnee in enumerate(DatasetList, 0):
     print("[%d] %s" % (cnt, listedonnee))
-print("\r")    
+print("\r\n")
     # CHOIX DU DATASET
 optdataset = int(input("Choisissez le dataset à tester [0-%s]: " % cnt))
 print("Dataset " + DatasetList[optdataset] + " sélectionné")
@@ -17,31 +28,31 @@ print("\r")
 optbatch_size = int(input("Choisissez le batch size à tester [0-%s]: " % cnt))
 print("Batch de " + Batch_sizeList[optbatch_size] + " sélectionné")
 optbatch_size = Batch_sizeList[optbatch_size]
-print("\r")  
+print("\r\n")  
     # FIN DU CHOIX DU BATCH SIZE
     
     # CHOIX DU NOMBRE D'EPOCH
 optn_epochs = int(input("Choisissez le nombre d'EPoch : "))
 print(str(optn_epochs) + " EPOCH sélectionnée")
-print("\r")    
+print("\r\n")    
     # FIN DU CHOIX DU NOMBRE D'EPOCH
     
     # CHOIX DU LABEL à GENERER
 optgenidlabel = int(input("Choisissez le label à générer [0-%s]: " % cnt))
-print("Dataset " + Labeldataset1List[optgenidlabel] + " sélectionné")
-print("\r")  
+print("DaLe label nommé " + Labeldataset1List[optgenidlabel] + " sélectionné")
+print("\r\n")  
     # FIN DU DU LABEL à GENERER
     
     # CHOIX DU NOMBRE à GENERER
 optgennumber = int(input("Entrer le nombre à générer : "))
 print("Le nombre \"" + str(optgennumber) + "\" va être généré")
-print("\r")      
+print("\r\n")      
     # FIN DU CHOIX DU NOMBRE
     
     # CHOIX DU NOMBRE D'INTERVAL à SAMPLER
 optsample_interval = int(input("Entrer l'interval de génération d'image : "))
 print(str(optsample_interval) + " va petre généré")
-print("\r")  
+print("\r\n")  
     # FIN DU DU NOMBRE D'INTERVAL à SAMPLER
 
 if optdataset == 0:
