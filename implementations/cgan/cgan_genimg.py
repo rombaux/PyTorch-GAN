74ac490f30ec80caaece0eb1870149b337f7b5b8
rombaux <rombaux@hotmail.com>
2019-08-06 21:39:03 +0200

    erer

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -205,7 +205,7 @@ if opt.dataset == 3:                # STL 10
             "../../data/STL10",
             split='train',
             transform=transforms.Compose(
-                [transforms.Resize(opt.img_size), transforms.ToTensor(),transforms.Normalize(( 0.4 , 0.4 , 0.4 ), ( 0.4 , 0.4 , 0.4 ))]
+                [transforms.Resize(opt.img_size), transforms.ToTensor(),transforms.Normalize(( 0.5 , 0.5 , 0.5 ), ( 0.5 , 0.5 , 0.5 ))]
             ),
             #target_transform=None,
             download=True
