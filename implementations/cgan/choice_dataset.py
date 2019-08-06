ae000480dbd3bbede0bc2f1c59d38edfaee8be64
rombaux <rombaux@hotmail.com>
2019-08-06 03:10:11 +0200

    dfd

--- implementations/cgan/choice_dataset.py
+++ implementations/cgan/choice_dataset.py
@@ -1,9 +1,9 @@
-fileList = [u"dataset 0 - MNIST",u"dataset 1 - CIFAR 10",u"dataset 2 - CIFAR 100",u"dataset 3 - STL 10",u"dataset 4 - Fashion MNIST",u"dataset 5 - ImageNet",u"dataset 6 - EMNIST"]
+DatasetList = [u"dataset 0 - MNIST",u"dataset 1 - CIFAR 10",u"dataset 2 - CIFAR 100",u"dataset 3 - STL 10",u"dataset 4 - Fashion MNIST",u"dataset 5 - ImageNet",u"dataset 6 - EMNIST"]
 
-for cnt,listedonnee in enumerate(fileList, 0):
+for cnt,listedonnee in enumerate(DatasetList, 0):
     print("[%d] %s" % (cnt, listedonnee))
 print("\r")    
 
 choice = int(input("Choisissez le dataset à tester [0-%s]: " % cnt))
 
-print("Dataset " + fileList[choice] + " sélectionné")
+print("Dataset " + DatasetList[choice] + " sélectionné")
