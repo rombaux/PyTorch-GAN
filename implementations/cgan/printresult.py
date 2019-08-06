c12ebee8ae70e584f87a4e7aa693b50d40865d23
rombaux <rombaux@hotmail.com>
2019-08-06 17:17:41 +0200

    dvfd

---
+++ implementations/cgan/printresult.py
@@ -0,0 +1,7 @@
+from IPython.display import Image
+print('All label')
+display(Image(filename='/content/gdrive/My Drive/TFE/dataset/modelimage/1.png'))
+print('Label Choisi')
+display(Image(filename='/content/gdrive/My Drive/TFE/dataset/modelimage/gen_label_id_1.png'))
+print('Mot Choisi')
+display(Image(filename='/content/gdrive/My Drive/TFE/dataset/modelimage/gen_multiple_1.png'))
