af296807eb958f000fdda8290a87b679a75ea2a9
rombaux <rombaux@hotmail.com>
2019-08-06 19:49:36 +0200

    gfg

--- implementations/cgan/printresult.py
+++ implementations/cgan/printresult.py
@@ -1,7 +1,7 @@
 from IPython.display import Image
 print('All label')
-display(Image(filename='/content/gdrive/My Drive/TFE/dataset/' + str(opt.dataset) + '/' + str(date_string) + '/' + 'modelimage/full.png'))
+display(Image(filename='/content/gdrive/My Drive/TFE/dataset/' + optdataset + '/' + str(date_string) + '/' + 'modelimage/full.png'))
 print('Label Choisi')
-display(Image(filename='/content/gdrive/My Drive/TFE/dataset' + str(opt.dataset) + '/' + str(date_string) + '/' + 'modelimage/gen_label_id_1.png'))
+display(Image(filename='/content/gdrive/My Drive/TFE/dataset' + str(optdataset) + '/' + str(date_string) + '/' + 'modelimage/gen_label_id_1.png'))
 print('Mot Choisi')
-display(Image(filename='/content/gdrive/My Drive/TFE/dataset' + str(opt.dataset) + '/' + str(date_string) + '/' + 'modelimage/gen_multiple_1.png'))
+display(Image(filename='/content/gdrive/My Drive/TFE/dataset' + str(optdataset) + '/' + str(date_string) + '/' + 'modelimage/gen_multiple_1.png'))
