4594d8bbc4164269ceca2eecefa86a7c43bcc839
rombaux <rombaux@hotmail.com>
2019-08-07 21:58:23 +0200

    fgfg

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -450,8 +450,7 @@ for epoch in range(opt.n_epochs):
             compteur = compteur + 1
             sample_image(n_row=opt.n_classes, batches_done=compteur, date_string=date_string)
             sample_label_id_image(n_row=opt.n_classes, batches_done=compteur, date_string=date_string)
-            print("image sauvée" + str(compteur) + ".png et batches_done = " + str(batches_done))
-            print("batches_done : " + str(batches_done))
+            print("image sauvée pour l'interval nu : " + str(compteur) + ".png et batches_done = " + str(batches_done))
  
     PATCH = "/content/gdrive/My Drive/TFE/dataset/"+str(opt.dataset)+"/"+date_string+"/model/"+"model_from_epoch_" + str(epoch) + ".pth"
     torch.save(generator.state_dict(), PATCH)
