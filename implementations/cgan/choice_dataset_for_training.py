a3ef4998a890f0ab1572972121f7d9f72075853f
rombaux <rombaux@hotmail.com>
2019-08-07 17:51:47 +0200

    fgf

--- implementations/cgan/choice_dataset_for_training.py
+++ implementations/cgan/choice_dataset_for_training.py
@@ -79,9 +79,9 @@ if optdataset == 6:
     optimg_size = 28    
 
     # CHOIX DE LA TAILLE D'IMAGE
-optimg_size = int(input("Entrer la taille de l'image par défaut le dataset est de " + str(optimg_size) + " pixel par " + str(optimg_size) + " : "))
+optimg_size = int(input(("Entrer la taille de l'image par défaut le dataset est de " + str(optimg_size) + " pixel par " + str(optimg_size) + " : ") or '32'))
 
-if not input("Entrer la taille de l'image par défaut le dataset est de "+ str(optimg_size) + " pixel par " + str(optimg_size) + " : ") :
+if not input("Entrer la taille de l'image (Défaut : "+ str(optimg_size) + "px x " + str(optimg_size) + "px : ") :
     optimg_size = optimg_size
 
 print("L'image va être resize en " + str(optimg_size) + " pixels")
