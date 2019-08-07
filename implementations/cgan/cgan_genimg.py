3aca850df43cbd2e2fd5628899ce576ef21bed2b
rombaux <rombaux@hotmail.com>
2019-08-07 11:59:52 +0200

    fgfgf

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -244,7 +244,7 @@ if opt.dataset == 5:
             transform=transforms.Compose(
                 [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize(( 0.5 , 0.5 , 0.5 ), ( 0.5 , 0.5 , 0.5 ))]
             ),
-            #target_transform=None,
+            target_transform=True,
             download=True
             ),
         
