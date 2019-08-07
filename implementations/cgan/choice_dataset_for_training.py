1b5aeb026d1c7fa957772d734f76a880b13092aa
rombaux <rombaux@hotmail.com>
2019-08-07 11:49:25 +0200

    fgfg

--- implementations/cgan/choice_dataset_for_training.py
+++ implementations/cgan/choice_dataset_for_training.py
@@ -18,7 +18,7 @@ Labeldataset1List = [u"Airplane",u"Auto",u"Bird",u"Cat",u"Deer",u"Dog",u"Frog",u
 
 Labeldataset3List = [u"Airplane",u"Bird",u"Auto",u"Cat",u"Deer",u"Dog",u"Horse",u"Monkey",u"Ship",u"Truck"]
 Labeldataset4List = [u"T-shirt/top",u"Trouser",u"Pullover",u"Dress",u"Coat",u"Sandal",u"Shirt",u"Sneaker",u"Bag",u"Ankle boot"]
-
+Labeldataset5List = [u"Aeroplane",u"Bicycle",u"Bird",u"Boat",u"Bottle",u"Bus",u"Car",u"Cat",u"Chair",u"Cow",u"Diningtable",u"Dog",u"Horse",u"Motorbike",u"Person",u"Pottedplant",u"Sheep",u"Sofa",u"Train",u"Tvmonitor"]
 Labeldataset6List = [u"Chiffre 0",u"Chiffre 1",u"Chiffre 2",u"Chiffre 3",u"Chiffre 4",u"Chiffre 5",u"Chiffre 6",u"Chiffre 7",u"Chiffre 8",u"Chiffre 9",
                     u"Lettre A",u"Chiffre B",u"Lettre C",u"Lettre D",u"Lettre E",u"Lettre F",u"Lettre G",u"Lettre H",u"Lettre I",u"Lettre J",
                     u"Lettre K",u"Lettre L",u"Lettre M",u"Lettre N",u"Lettre O",u"Lettre P",u"Lettre Q",u"Lettre R",u"Lettre S",u"Lettre T",
