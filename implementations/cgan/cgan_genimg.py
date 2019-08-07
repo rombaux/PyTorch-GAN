a8e71fcc49b89ff7d95c93a306f1812786f47b69
rombaux <rombaux@hotmail.com>
2019-08-07 21:33:39 +0200

    jhjh

--- implementations/cgan/cgan_genimg.py
+++ implementations/cgan/cgan_genimg.py
@@ -445,25 +445,18 @@ for epoch in range(opt.n_epochs):
 
             plt.close('all')
 
-# --------->indentation pour sample par batch
-'''
-            sample_image(n_row=opt.n_classes, batches_done=batches_done, date_string=date_string)
-            sample_label_id_image(n_row=opt.n_classes, batches_done=batches_done, date_string=date_string)
-            print("batches_done : " + str(batches_done))
-            print("len(dataloader) : " + str(len(dataloader)))
-            print("epoch : " + str(epoch))
-            print("i : " + str(i))
-'''
-    PATCH = "/content/gdrive/My Drive/TFE/dataset/"+str(opt.dataset)+"/"+date_string+"/model/"+"model_epoch_" + str(epoch) + ".pth"
-    torch.save(generator.state_dict(), PATCH)
-    print("Model saved in "+str(PATCH))
+# --------->indentation pour  sample par batch 
+        sample_image(n_row=opt.n_classes, batches_done=batches_done, date_string=date_string)
+        sample_label_id_image(n_row=opt.n_classes, batches_done=batches_done, date_string=date_string)
+        print("batches_done : " + str(batches_done))
+        print("len(dataloader) : " + str(len(dataloader)))
+        print("epoch : " + str(epoch))
+        print("i : " + str(i))
 
-    sample_image(n_row=opt.n_classes, batches_done=epoch, date_string=date_string)
-    sample_label_id_image(n_row=opt.n_classes, batches_done=epoch, date_string=date_string)
-    print("batches_done : " + str(batches_done))
-    print("len(dataloader) : " + str(len(dataloader)))
-    print("epoch : " + str(epoch))
-    print("i : " + str(i))
+
+    PATCH = "/content/gdrive/My Drive/TFE/dataset/"+str(opt.dataset)+"/"+date_string+"/model/"+"model_" + str(batches_done) + ".pth"
+    torch.save(generator.state_dict(), PATCH)
+    print("Model saved in "+str(PATCH))  
 
 
 
