8b7362c3b65bb5868e9055dd9e8de42837ebc309
rombaux <rombaux@hotmail.com>
2019-08-08 15:37:34 +0200

    fdf

--- implementations/cgan/cgan_loadgenimg.py
+++ implementations/cgan/cgan_loadgenimg.py
@@ -107,8 +107,8 @@ def sample_image(n_row, batches_done,date_string):
     labels = Variable(LongTensor(labels))
     gen_imgs = generator(z, labels)
     save_image(gen_imgs.data, b + "/modelimage/full_" + date_string + "_%s.png" % (str(batches_done).zfill(3), nrow=n_row, normalize=True)
-    src = b + '/modelimage/full_' + date_string + '_1.png'
-    dst = '/content/gdrive/My Drive/TFE/dataset/modelimage/full_1.png'
+    src = b + '/modelimage/full_' + date_string + '_001.png'
+    dst = '/content/gdrive/My Drive/TFE/dataset/modelimage/full_001.png'
     copyfile(src,dst)
 
 def sample_label_id_image(n_row, batches_done,date_string):
@@ -121,8 +121,8 @@ def sample_label_id_image(n_row, batches_done,date_string):
     gen_imgs = generator(z, labels)
     if opt.genidlabel < 10:
         save_image(gen_imgs.data[opt.genidlabel], b + "/modelimage/gen_label_" + str(opt.genidlabel) + "_" + date_string + "_%s.png" % (str(batches_done).zfill(3), nrow=n_row, normalize=True)
-        src = b + '/modelimage/gen_label_' + str(opt.genidlabel) + '_' + date_string + '_1.png'
-        dst = '/content/gdrive/My Drive/TFE/dataset/modelimage/gen_label_id_1.png'
+        src = b + '/modelimage/gen_label_' + str(opt.genidlabel) + '_' + date_string + '_001.png'
+        dst = '/content/gdrive/My Drive/TFE/dataset/modelimage/gen_label_id_100.png'
         copyfile(src,dst)
         print("label : "+str(opt.genidlabel)+" generated")
     if opt.gennumber > 0:
@@ -131,8 +131,8 @@ def sample_label_id_image(n_row, batches_done,date_string):
         for a in str(toto):
             numbre.append(gen_imgs.data[int(a)])
         save_image(numbre, b + "/modelimage/gen_number_" + str(opt.gennumber) + "_" + date_string + "_%s.png" % (str(batches_done).zfill(3), nrow=n_row, normalize=True)
-        src = b +'/modelimage/gen_number_' + str(opt.gennumber) + '_' + date_string + '_1.png'
-        dst = '/content/gdrive/My Drive/TFE/dataset/modelimage/gen_multiple_1.png'
+        src = b +'/modelimage/gen_number_' + str(opt.gennumber) + '_' + date_string + '_001.png'
+        dst = '/content/gdrive/My Drive/TFE/dataset/modelimage/gen_multiple_001.png'
         copyfile(src,dst)        
         print("nombre : "+str(opt.gennumber)+" generated")
 
