57360853c65cfc34f90624482b83472c6e8b3f30
rombaux <rombaux@hotmail.com>
2019-08-12 19:39:12 +0200

    vbv

--- implementations/cgan/menu_train.py
+++ implementations/cgan/menu_train.py
@@ -22,7 +22,7 @@ print(colored("-----------------------------" + date_string[11:16] + "----------
 print(colored("---------------------------------------------------------------","green"))
 print(colored("---                  ENTRAINEMENT DU MODELE                 ---","green"))
 print(colored("---------------------------------------------------------------","green"))
-display(Image(filename='/content/PyTorch-GAN/assets/reseau.jpg',width=450,height=217))
+display(Image(filename='/content/PyTorch-GAN/assets/reseau.jpg',width=512,height=247))
 print(colored("---------------------------------------------------------------","green"))
 print(colored("---                  MENU DE CONFIGURATION                  ---","green"))
 print(colored("---------------------------------------------------------------","green"))
