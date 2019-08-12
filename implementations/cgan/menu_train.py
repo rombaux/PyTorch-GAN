778c4f40a3ffe432c9d62ee4a73339f02d8cb243
rombaux <rombaux@hotmail.com>
2019-08-12 18:15:53 +0200

    gf

--- implementations/cgan/menu_train.py
+++ implementations/cgan/menu_train.py
@@ -88,7 +88,7 @@ print("\r\n")
 
 # CHOIX DU BATCH SIZE
 for cnt,listebatch in enumerate(Batch_sizeList, 0):
-    print("[%d] %s" % (cnt, listebatch) + " soit " + int(DatasetSize[cnt]) )     
+    print("[%d] %s" % (cnt, listebatch) + " soit " + (DatasetSize[cnt]) )     
 optbatch_size = int(input("Choisissez le batch size à tester [0-%s]: " % cnt))
 print("Batch de " + Batch_sizeList[optbatch_size] + " sélectionné")
 optbatch_size = Batch_sizeList[optbatch_size]
