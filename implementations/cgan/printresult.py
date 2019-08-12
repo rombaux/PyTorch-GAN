31dec925df2fe362eb63f8e4da408797a554e142
rombaux <rombaux@hotmail.com>
2019-08-12 22:24:51 +0200

    df

--- implementations/cgan/printresult.py
+++ implementations/cgan/printresult.py
@@ -3,5 +3,6 @@ import time
 time.sleep(2)
 print('\r\nGrille de tous les labels du dataset')
 display(Image(filename='/content/gdrive/My Drive/TFE/dataset/modelimage/full_0001.png'))
-print('\r\nMot Choisi : ' + str(gennumber))
+print('\r\nMot Choisi : ')
+print("Suite : "+str(opt.gennumber)+" générée")
 display(Image(filename='/content/gdrive/My Drive/TFE/dataset/modelimage/gen_multiple_0001.png'))
