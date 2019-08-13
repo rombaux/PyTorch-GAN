af6e4c5383ce9f548ea58b44eec2a247f515e798
rombaux <rombaux@hotmail.com>
2019-08-14 00:07:17 +0200

    edfd

--- implementations/cgan/cgan_generate.py
+++ implementations/cgan/cgan_generate.py
@@ -163,9 +163,8 @@ for idx,DirName in enumerate(DirList):
             fntemp.append(os.path.join(base, file))
             fntemp.reverse() 
 #            fn.append(os.path.join(base, file))
-        for filepth in range(0,9 or len(fntemp))
+        for filepth in range(0,9)
             fn.append(filepth)
-     
             
 fileList = [name for name in fn if name.endswith(".pth")]
 
