01aafce0e246916ea1c947f2b04c758ada613676
rombaux <rombaux@hotmail.com>
2019-08-13 20:42:19 +0200

    dsds

--- implementations/cgan/cgan_train.py
+++ implementations/cgan/cgan_train.py
@@ -103,8 +103,8 @@ class Generator(nn.Module):
         print( "Model's state_dict : " )
         for param_tensor in self.model.state_dict ():
             print(param_tensor , " \t " , self.model.state_dict ()[ param_tensor ] . size ())
-        print("module.state_dict().keys() : ")     
-        print(self.state_dict().keys())
+        print("module.state_dict().keys(model.0.bias) : ")     
+        print(self.state_dict().keys(model.0.bias))
         print("self._state_dict_hooks.values : ")
         print(self._state_dict_hooks.values())
         
