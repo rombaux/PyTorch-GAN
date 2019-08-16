94cf7a0f488822ff42854331cc635d1fc4fb99e8
rombaux <rombaux@hotmail.com>
2019-08-16 18:49:39 +0200

    fgfg

--- implementations/cgan/cgan_generate.py
+++ implementations/cgan/cgan_generate.py
@@ -33,6 +33,9 @@ parser.add_argument("--dataset", type=int, default=0, help="choice of dataset - 
 opt = parser.parse_args()
 print(opt)
 
+print("le dataset choisi est " + str(optdataset))
+print("le mot choisi est " + optgenword) 
+
 dictotodataset0 = { '0':'0', '1':'1', '2':'2', '3':'3', '4':'4', '5':'5', '6':'6', '7':'7', '8':'8', '9':'9'}
 dictotodataset5 = { '0':'0', '1':'1', '2':'2', '3':'3', '4':'4', '5':'5', '6':'6', '7':'7', '8':'8', '9':'9',
                     'A':'10','B':'11','C':'12','D':'13','E':'14','F':'15','G':'16','H':'17','I':'18','J':'19',
