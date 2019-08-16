cac406c6b647fb18d246d288d12ab34f902ead3e
rombaux <rombaux@hotmail.com>
2019-08-16 21:34:07 +0200

    ffg

--- implementations/cgan/printresult.py
+++ implementations/cgan/printresult.py
@@ -2,6 +2,7 @@ from IPython.display import Image
 import time
 time.sleep(5)
 print('\r\nGrille de tous les labels du dataset')
-display(Image(filename='/content/gdrive/My Drive/TFE/dataset/modelimage/full_0001.png'))
+if optdataset != 2:
+    display(Image(filename='/content/gdrive/My Drive/TFE/dataset/modelimage/full_0001.png'))
 print('\r\nMot Choisi : ')
 display(Image(filename='/content/gdrive/My Drive/TFE/dataset/modelimage/gen_multiple_0001.png'))
