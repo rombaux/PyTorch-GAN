33428c6f2ca53cc95804029985d3e8d09eadd0ea
rombaux <rombaux@hotmail.com>
2019-08-16 21:26:34 +0200

    dfd

--- implementations/cgan/printresult.py
+++ implementations/cgan/printresult.py
@@ -1,6 +1,6 @@
 from IPython.display import Image
 import time
-time.sleep(2)
+time.sleep(5)
 print('\r\nGrille de tous les labels du dataset')
 display(Image(filename='/content/gdrive/My Drive/TFE/dataset/modelimage/full_0001.png'))
 print('\r\nMot Choisi : ')
